120119e886c3b6b57a078053b244865202ba0b3e
Eric McDaniel <McDanielES@gmail.com>
2020-04-10 11:28:30 -0500

    Hide output at startup, begin generating problem set

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ $("#encryptAction").on("click", function() {
   $(this).parentsUntil(".inner-container").slideUp(200, function() {
     $(this).remove();
   });
+  $("#output").css("display", "block");
   var difficulty = $("input[name='level']:checked").val();
   var message = $("input[name='message']").val();
+  var problems = generateProblemSet(message.length);
+  console.log(problems);
+
+  // $("#eq").each(function( index ) {
+  //   console.log( index + ": " + $( this ).text() );
+  // });
+  $("#eq").text("3+3=");
 });
+
+function generateProblemSet(length) {
+  return length;
+}
