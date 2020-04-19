278b737a8799c61e2849c4e2a38939bf32734824
Eric McDaniel <McDanielES@gmail.com>
2020-04-19 00:31:18 -0500

    Implement multiply/divide functionality, pretesting

--- js/main.js
+++ js/main.js
@@ -15,14 +15,13 @@ const demoExamples = ["Ex: The moon is made of cheese!",
                       "Ex: Daddy snores in his sleep",
                       "Ex: The dog really ate my homework",
                       "Ex: No more monkeys jumping!"];
-const operations    = ["+", "-", "*", "/"];
+$("#message").attr("placeholder", demoExamples[Math.floor(demoExamples.length * Math.random())]);
+const operations    = ["+", "-", "×", "÷"];
 const colors        = ["#bb4455", "#2d3eb1", "#3d723a", "#bf6919"];
-const solutionRange = [1.5, 5.0, 5.0];
+const solutionRange = [1.5, 10, 15, 120];
 var   solutionsMap  = [];
 var   message       = "";
 var   difficulty;
-
-$("#message").attr("placeholder", demoExamples[Math.floor(demoExamples.length * Math.random())]);
 
 /**
  *                           #encryptAction - The Main Focus
@@ -55,14 +54,23 @@ $("#encryptAction").on("click", function() {
   }  
 });
 
+/**
+ * Builds a new problem set by doing so piece by piece, where the values are kept
+ * in parallel arrays, and its construction is dependant on difficulty level. Addition
+ * and subtraction problems work backwards generating a solution first and randomly place
+ * appropriate values that work, and multiplication/ division problems work inversely.
+ * 
+ * @param {Object} problems the complete set of problems including all terms, solutions, and
+ *                          assigned letters.
+ */
 function generateProblemSet() {
   // Instantiate empty "problems" object with parallel arrays
   var problems = { 
     letter: Array.from(message),
-    firstTerm: [],
-    operation: [],
+    firstTerm:  [],
+    operation:  [],
     secondTerm: [],
-    solution: []
+    solution:   []
   };
   
   generateSolutionsMap();
@@ -70,7 +78,10 @@ function generateProblemSet() {
 
   if (difficulty < 3) {
     populateAddSubtractSolutions(problems);
-    populateTerms(problems);
+    populateAddSubtractTerms(problems);
+  }
+  else {
+    populateMultiDivideProblems(problems);
   }
 
   return problems;
@@ -80,7 +91,8 @@ function generateProblemSet() {
  * Starts with the end in mind by generating the final answer first with a factor of a 
  * constant on the difficulty level chosen.
  * 
- * @param {Object} problems 
+ * @param {Object} problems the complete set of problems including all terms, solutions, and
+ *                          assigned letters.
  */
 function populateAddSubtractSolutions(problems) {
   for (var i = 0; i < message.length; i++) {
@@ -98,9 +110,10 @@ function populateAddSubtractSolutions(problems) {
  * the difference of the solution and the first term. Looping until the random seed is > 0.5 is
  * necessary to prevent negative differences.
  * 
- * @param {Object} problems 
+ * @param {Object} problems the complete set of problems including all terms, solutions, and
+ *                          assigned letters.
  */
-function populateTerms(problems) {
+function populateAddSubtractTerms(problems) {
   console.log("Printed List for Debugging Purposes. This does not alter performance of application.");
   for (var i = 0; i < message.length; i++) {
     // if the problem set is addition, take the difference of the solution and the first term
@@ -125,6 +138,39 @@ function populateTerms(problems) {
   }
 }
 
+function populateMultiDivideProblems(problems) {
+  for (var i = 0; i < message.length; i++) {
+    var first, second;
+    if (problems.operation[i] === "×") {
+      first = (Math.floor(Math.random() * solutionRange[2]));
+      second = (Math.floor(Math.random() * solutionRange[2]) + 1);
+      while (problems.solution.includes(first * second)) {
+        first++;
+        // console.log("first:" + first + problems.operation[i] + "   second: " + second);
+      }
+      problems.solution[i] = first * second;
+    }
+    else {
+      do {
+        first = (Math.floor(Math.random() * solutionRange[3]));
+        second = (Math.floor(Math.random() * 9) + 1);
+        while (first % second !== 0) {
+          first++;
+          // console.log("first:" + first + "   second: " + second);
+        }
+      } while (problems.solution.includes(first / second));
+      problems.solution[i] = first / second;
+    }
+    problems.firstTerm[i] = first;
+    problems.secondTerm[i] = second;
+
+    // Printed List for Debugging Purposes
+    if (problems.letter[i] !== " ")
+    console.log(problems.letter[i] + " ||| " + problems.firstTerm[i] + " "
+        + problems.operation[i] + " " + problems.secondTerm[i] + " = " + problems.solution[i]);
+  }
+}
+
 /**
  * A random collection of +, -, *, and / operations are created and pushed into the operations
  * array of the problems object. The user's difficulty level is taken into account, omitting
@@ -136,11 +182,18 @@ function populateTerms(problems) {
 function populateOperations(problems) {  
   for (var i = 0; i < message.length; i++) {
     problems.operation.push( (difficulty === 3) ? 
-                              operations[Math.floor(Math.random() * (operations.length - 2)) + 2] :
+                              operations[Math.floor((Math.random() * 0.75) * (operations.length - 2)) + 2] :
                               operations[Math.floor(Math.random() * (operations.length - 2))]);
   }
 }
 
+/**
+ * Prints out the formatted problem set, mapped with their corresponding shuffled counterparts,
+ * into the document window. Spaces are kept blank by discarding the problem entirely. ¯\_(ツ)_/¯
+ * 
+ * @param {Object} problems the complete set of problems including all terms, solutions, and
+ *                          assigned letters.
+ */
 function printProblemSet(problems) {
   // Iterate through each shuffled letter, skipping spaces, and use DOM manipulation to add a new
   // div and fill it contents with the created math expressions.
@@ -155,6 +208,13 @@ function printProblemSet(problems) {
   }
 }
 
+/**
+ * Prints out the answer bubble at the bottom with the solutions with the overbar to allow the
+ * corresponding letter to be written above. Spaces have no overbar.
+ * 
+ * @param {Object} problems the complete set of problems including all terms, solutions, and
+ *                          assigned letters.
+ */
 function printAnswerBubble(problems) {
   $("#output").append("<div class='row'></div>");
   $(".row").last().append("<div class='col-xl-1 col-md-1 col-sm-12 mt-3'></div>");
@@ -173,6 +233,10 @@ function printAnswerBubble(problems) {
     }
 }
 
+/**
+ * A helper function that creates the global solutions map using the Fisher-Yates
+ * shuffle function below.
+ */
 function generateSolutionsMap() {
   var map = [];
   for (var i = 0; i < message.length; i++) {
@@ -186,7 +250,7 @@ function generateSolutionsMap() {
  * reinvent the wheel if it works well. This is a generic helper function to shuffle the letters
  * around and not make the message easy to solve.
  * 
- * @param Array arr the original array presorted
+ * @param arr arr the original array presorted
  * @return arr the modified array after it is shuffled
  */
 function shuffle(arr) {
